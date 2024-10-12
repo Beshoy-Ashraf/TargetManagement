6fd7154aeae89aecaeaffc4dba309d0c6d6dce6c
Beshoy-Ashraf <bishoy.ashraf.a046@gmail.com>
2024-10-12 20:50:58 +0300

    culcu achieve

--- lib/layout/cubit/AppCubit.dart
+++ lib/layout/cubit/AppCubit.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -374,6 +376,120 @@ class Appcubit extends Cubit<AppStates> {
     } catch (error) {
       print('Error getting target data for day: $error');
       emit(GetDataError());
+    }
+  }
+
+  Future<Object> calculateAchie(String userId) async {
+    double totalPoints = 0.0 as double;
+    double totalGA = 0.0 as double;
+    double totalOC = 0.0 as double;
+    double totalHome4G = 0.0 as double;
+    double totalDevices = 0.0 as double;
+    double totalDsl = 0.0 as double;
+    double pointKpi = 0.0 as double;
+    double OCKpi = 0.0 as double;
+    double DSLKpi = 0.0 as double;
+    double GAKpi = 0.0 as double;
+    double DevicesKpi = 0.0 as double;
+    double Home4GKpi = 0.0 as double;
+    double point = 0.0 as double;
+    double GA = 0.0 as double;
+    double OC = 0.0 as double;
+    double DSL = 0.0 as double;
+    double Home4G = 0.0 as double;
+    double Devices = 0.0 as double;
+    double pointper = 0.0 as double;
+    double GAper = 0.0 as double;
+    double OCper = 0.0 as double;
+    double DSLper = 0.0 as double;
+    double Home4Gper = 0.0 as double;
+    double Devicesper = 0.0 as double;
+    try {
+      //   QuerySnapshot querySnapshot = await FirebaseFirestore.instance
+      //       .collection('users')
+      //       .doc(userId)
+      //       .collection('initialTarget')
+      //       .get();
+
+      //   for (var doc in querySnapshot.docs) {
+      //     if (doc.exists) {
+      //       Map<String, dynamic>? data = doc.data() as Map<String, dynamic>?;
+      //       point = (data?['points'] as double?)!;
+      //       GA = (data?['GA'] as double?)!;
+      //       OC = (data?['orangeCash'] as double?)!;
+      //       DSL = (data?['adsl'] as double?)!;
+      //       Devices = (data?['devices'] as double?)!;
+      //       Home4G = (data?['home4g'] as double?)!;
+      //       pointKpi = (data?['pointper'] as double?)!;
+      //       GAKpi = (data?['GAper'] as double?)!;
+      //       OCKpi = (data?['orangeCashper'] as double?)!;
+      //       DSLKpi = (data?['adslper'] as double?)!;
+      //       DevicesKpi = (data?['devicesper'] as double?)!;
+      //       Home4GKpi = (data?['home4gper'] as double?)!;
+      //     }
+      //   }
+      //   querySnapshot = await FirebaseFirestore.instance
+      //       .collection('users')
+      //       .doc(userId)
+      //       .collection('totaltarget')
+      //       .get();
+
+      //   for (var doc in querySnapshot.docs) {
+      //     if (doc.exists) {
+      //       Map<String, dynamic>? data = doc.data() as Map<String, dynamic>?;
+      //       totalPoints = (data?['totalPoints'] as double?)!;
+      //       totalGA = (data?['totalGA'] as double?)!;
+      //       totalOC = (data?['totalOC'] as double?)!;
+      //       totalDsl = (data?['totalDsl'] as double?)!;
+      //       totalDevices = (data?['totalDevices'] as double?)!;
+      //       totalHome4G = (data?['totalHome4G'] as double?)!;
+      //     }
+      // }
+
+      pointper = (totalPoints * 100 / point) as double;
+      pointKpi = (pointper * pointKpi / 100) as double;
+      GAper = (totalGA * 100 / GA) as double;
+      GAKpi = 0.0;
+      OCper = (totalOC * 100 / OC) as double;
+      OCKpi = (OCper * OCKpi / 100) as double;
+      DSLper = (totalDsl * 100 / DSL) as double;
+      DSLKpi = (DSLper * DSLKpi / 100) as double;
+      Devicesper = (totalDevices * 100 / Devices) as double;
+      DevicesKpi = (Devicesper * DevicesKpi / 100) as double;
+      Home4Gper = (totalHome4G * 100 / Home4G) as double;
+      Home4GKpi = (Home4Gper * Home4GKpi / 100) as double;
+      print(pointKpi);
+      Map<String, dynamic> m = {
+        'pointper': pointper,
+        'pointKpi': pointKpi,
+        'GAper': GAper,
+        'GAKpi': GAKpi,
+        'Home4Gper': Home4Gper,
+        'Home4GKpi': Home4GKpi,
+        'Devicesper': Devicesper,
+        'DevicesKpi': DevicesKpi,
+        'OCper': OCper,
+        'OCKpi': OCKpi,
+        'DSLper': DSLper,
+        'DSLKpi': DSLKpi,
+      };
+      print(m);
+      FirebaseFirestore.instance
+          .collection('users')
+          .doc(userId)
+          .collection('totalAche')
+          .doc('Ache')
+          .set(m)
+          .then((value) {
+        print('done');
+        emit(SetDataSuccessful());
+      }).catchError((error) {
+        emit(SetDataError());
+      });
+      return m;
+    } catch (error) {
+      print('Error calculating total points: $error');
+      return 0;
     }
   }
 }
--- lib/modules/Achievement/Achievement.dart
+++ lib/modules/Achievement/Achievement.dart
@@ -17,96 +17,353 @@ class Achievement extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     int value = 70;
-    double percent = value / 100;
+    int totalPoints = t.point;
+    int totalGA = t.GA;
+    int totalOC = t.orangeCash;
+    int totalHome4G = t.home4g;
+    int totalDevices = t.devices;
+    int totalDsl = t.adsl;
+    int pointKpi = initAch.pointper;
+    int OCKpi = initAch.orangeCashper;
+    int DSLKpi = initAch.adslper;
+    int GAKpi = initAch.GAper;
+    int DevicesKpi = initAch.devicesper;
+    int Home4GKpi = initAch.home4gper;
+    int point = initAch.point;
+    int GA = initAch.GA;
+    int OC = initAch.orangeCash;
+    int DSL = initAch.adsl;
+    int Home4G = initAch.home4g;
+    int Devices = initAch.devices;
+    int pointper = 0;
+    int GAper = 0;
+    int OCper = 0;
+    int DSLper = 0;
+    int Home4Gper = 0;
+    int Devicesper = 0;
+    pointper = (point != 0) ? ((totalPoints * 100 / point).toInt()) : 0;
+    pointKpi = (pointper * pointKpi / 100).toInt();
+    GAper = (GA != 0) ? ((totalGA * 100 / GA).toInt()) : 0;
+    OCper = (OC != 0) ? ((totalOC * 100 / OC).toInt()) : 0;
+    OCKpi = (OCper * OCKpi / 100).toInt();
+    DSLper = (DSL != 0) ? ((totalDsl * 100 / DSL).toInt()) : 0;
+    DSLKpi = (DSLper * DSLKpi / 100).toInt();
+    Devicesper = (Devices != 0) ? ((totalDevices * 100 / Devices).toInt()) : 0;
+    DevicesKpi = (Devicesper * DevicesKpi / 100).toInt();
+    Home4Gper = (Home4G != 0) ? ((totalHome4G * 100 / Home4G).toInt()) : 0;
+    Home4GKpi = (Home4Gper * Home4GKpi / 100).toInt();
     return BlocConsumer<Appcubit, AppStates>(
       listener: (context, state) {},
       builder: (context, state) {
         return Scaffold(
-          body: Padding(
-            padding: const EdgeInsets.only(top: 50, left: 20, right: 20),
-            child: Center(
-              child: Column(
-                children: [
-                  Expanded(
-                    child: ListView.separated(
-                      itemBuilder: (context, index) {
-                        return itemBild(
-                          context: context,
-                          total: t,
-                          init: initAch,
-                          title: titels[index],
-                          index: index,
-                        );
-                      },
-                      separatorBuilder: (context, index) {
-                        return SizedBox(
-                          height: 10,
-                        );
-                      },
-                      itemCount: titels.length,
+          body: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 50, left: 20, right: 20),
+              child: Center(
+                child: Column(
+                  children: [
+                    Card(
+                      color: Theme.of(context).cardTheme.color,
+                      clipBehavior: Clip.antiAliasWithSaveLayer,
+                      elevation: 10,
+                      child: Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Container(
+                          width: double.infinity,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                width: double.infinity,
+                                child: Column(
+                                  children: [
+                                    Text(
+                                      'point',
+                                      style: TextStyle(
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.w700),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              LinearProgressIndicator(
+                                value: pointper / 100,
+                              ),
+                              Center(child: Text('${(pointper).toString()}%')),
+                              Text('remain : ${((100 - pointper)).toInt()} % '),
+                              SizedBox(
+                                width: 5,
+                              ),
+                              Text(
+                                  'remain : ${(point - totalPoints)} points  '),
+                              SizedBox(
+                                width: 5,
+                              ),
+                              Text('achieve : ${(totalPoints)} points'),
+                            ],
+                          ),
+                        ),
+                      ),
                     ),
-                  )
-                ],
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Card(
+                      color: Theme.of(context).cardTheme.color,
+                      clipBehavior: Clip.antiAliasWithSaveLayer,
+                      elevation: 10,
+                      child: Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Container(
+                          width: double.infinity,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                width: double.infinity,
+                                child: Column(
+                                  children: [
+                                    Text(
+                                      'lines',
+                                      style: TextStyle(
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.w700),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              LinearProgressIndicator(
+                                value: GAper / 100,
+                              ),
+                              Center(child: Text('${(GAper).toString()}%')),
+                              Text('remain : ${((100 - GAper)).toInt()} % '),
+                              SizedBox(
+                                width: 5,
+                              ),
+                              Text('remain : ${(GA - totalGA)} lines  '),
+                              SizedBox(
+                                width: 5,
+                              ),
+                              Text('achieve : ${(totalGA)} liens'),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Card(
+                      color: Theme.of(context).cardTheme.color,
+                      clipBehavior: Clip.antiAliasWithSaveLayer,
+                      elevation: 10,
+                      child: Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Container(
+                          width: double.infinity,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                width: double.infinity,
+                                child: Column(
+                                  children: [
+                                    Text(
+                                      'Orange Cash',
+                                      style: TextStyle(
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.w700),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              LinearProgressIndicator(
+                                value: OCper / 100,
+                              ),
+                              Center(child: Text('${(OCper).toString()}%')),
+                              Text('remain : ${((100 - OCper)).toInt()} % '),
+                              SizedBox(
+                                width: 5,
+                              ),
+                              Text('remain : ${(OC - totalOC)} walets  '),
+                              SizedBox(
+                                width: 5,
+                              ),
+                              Text('achieve : ${(totalOC)} walets'),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Card(
+                      color: Theme.of(context).cardTheme.color,
+                      clipBehavior: Clip.antiAliasWithSaveLayer,
+                      elevation: 10,
+                      child: Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Container(
+                          width: double.infinity,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                width: double.infinity,
+                                child: Column(
+                                  children: [
+                                    Text(
+                                      'Home 4G',
+                                      style: TextStyle(
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.w700),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              LinearProgressIndicator(
+                                value: Home4Gper / 100,
+                              ),
+                              Center(child: Text('${(Home4Gper).toString()}%')),
+                              Text(
+                                  'remain : ${((100 - Home4Gper)).toInt()} % '),
+                              SizedBox(
+                                width: 5,
+                              ),
+                              Text(
+                                  'remain : ${(Home4G - totalHome4G)} Home 4G  '),
+                              SizedBox(
+                                width: 5,
+                              ),
+                              Text('achieve : ${(totalHome4G)} Home 4G'),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Card(
+                      color: Theme.of(context).cardTheme.color,
+                      clipBehavior: Clip.antiAliasWithSaveLayer,
+                      elevation: 10,
+                      child: Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Container(
+                          width: double.infinity,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                width: double.infinity,
+                                child: Column(
+                                  children: [
+                                    Text(
+                                      'DSL',
+                                      style: TextStyle(
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.w700),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              LinearProgressIndicator(
+                                value: DSLper / 100,
+                              ),
+                              Center(child: Text('${(DSLper).toString()}%')),
+                              Text('remain : ${((100 - DSLper)).toInt()} % '),
+                              SizedBox(
+                                width: 5,
+                              ),
+                              Text(
+                                  'remain : ${((DSL - totalDsl) < 0 ? 0 : (DSL - totalDsl))} DSL  '),
+                              SizedBox(
+                                width: 5,
+                              ),
+                              Text('achieve : ${(totalDsl)} DSL'),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Card(
+                      color: Theme.of(context).cardTheme.color,
+                      clipBehavior: Clip.antiAliasWithSaveLayer,
+                      elevation: 10,
+                      child: Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Container(
+                          width: double.infinity,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                width: double.infinity,
+                                child: Column(
+                                  children: [
+                                    Text(
+                                      'Devices',
+                                      style: TextStyle(
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.w700),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              LinearProgressIndicator(
+                                value: Devicesper / 100,
+                              ),
+                              Center(
+                                  child: Text('${(Devicesper).toString()}%')),
+                              Text(
+                                  'remain : ${((100 - Devicesper)).toInt()} % '),
+                              SizedBox(
+                                width: 5,
+                              ),
+                              Text(
+                                  'remain : ${(Devices - totalDevices)} Devices  '),
+                              SizedBox(
+                                width: 5,
+                              ),
+                              Text('achieve : ${(totalDevices)} Devices'),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
         );
       },
-    );
-  }
-
-  List<String> titels = [
-    'Point',
-    'GA',
-    'DSL',
-    'HOME 4G',
-    'Devices',
-    'Orange Cash',
-  ];
-
-  Widget itemBild({
-    required BuildContext context,
-    required String title,
-    required int index,
-    required Total total,
-    required initialTarget init,
-  }) {
-    return Card(
-      color: Theme.of(context).cardTheme.color,
-      clipBehavior: Clip.antiAliasWithSaveLayer,
-      elevation: 10,
-      child: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Container(
-          width: double.infinity,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                width: double.infinity,
-                child: Column(
-                  children: [
-                    Text(
-                      title,
-                      style:
-                          TextStyle(fontSize: 15, fontWeight: FontWeight.w700),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              LinearProgressIndicator(
-                value: 200,
-              ),
-              Center(child: Text('${(200 * 100).toString()}%')),
-              Text('remain : ${((1 - 200) * 100).toInt()} % : '),
-              Text(init.GA.toString())
-            ],
-          ),
-        ),
-      ),
     );
   }
 }
